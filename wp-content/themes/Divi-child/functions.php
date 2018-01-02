fedc7c821a47fac9821aeae1d06ed68bc7c1e12a
Shawn Flock <sflock@navicamls.net>
2018-01-02 16:30:21 -0500

    stuff

--- wp-content/themes/Divi-child/functions.php
+++ wp-content/themes/Divi-child/functions.php
@@ -8,6 +8,8 @@ if ( !defined( 'ABSPATH' ) ) exit;
 if ( !function_exists( 'chld_thm_cfg_parent_css' ) ):
     function chld_thm_cfg_parent_css() {
         wp_enqueue_style( 'chld_thm_cfg_parent', trailingslashit( get_template_directory_uri() ) . 'style.css', array(  ) );
+        wp_enqueue_script('particle-js-min', get_stylesheet_directory_uri() . '/js/particles.min.js',array('jquery'),1.1,true);
+        wp_enqueue_script('main-js', get_stylesheet_directory_uri() . '/js/main.js',array('jquery'),1.1,true);
     }
 endif;
 add_action( 'wp_enqueue_scripts', 'chld_thm_cfg_parent_css', 10 );
